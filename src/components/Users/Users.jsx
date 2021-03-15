e99d45cad1b13a885d542bd73672dc97df6c6bcb
Israfil98 <israfil.guseynov.98@inbox.ru>
2021-03-15 15:45:57 +0400

    add axios library and sent get request in users page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,57 +2,43 @@ import React from 'react';
 
 import s from "./Users.module.css"
 import userPhoto from "../../assets/img/user.png";
+import axios from "axios";
+
+// {
+//     id: 1,
+//         isFollowed: false,
+//     location: {
+//     city: "Dubai",
+//         country: "OAE"
+// },
+//     status: "Hello, i am looking for a job",
+//         userPhoto: userPhoto,
+//     userName: "UserName1",
+// },
 
 const Users = (props) => {
     if (props.usersData.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                isFollowed: false,
-                location: {
-                    city: "Dubai",
-                    country: "OAE"
-                },
-                status: "Hello, i am looking for a job",
-                userPhoto: userPhoto,
-                userName: "UserName1",
-            },
-            {
-                id: 2,
-                isFollowed: true,
-                location: {
-                    city: "Washington",
-                    country: "USA"
-                },
-                status: "Hello, i am looking for a job",
-                userPhoto: userPhoto,
-                userName: "UserName2",
-            },
-            {
-                id: 3,
-                isFollowed: false,
-                location: {
-                    city: "Moscow",
-                    country: "Russia"
-                },
-                status: "Hello, i am looking for a job",
-                userPhoto: userPhoto,
-                userName: "UserName3",
-            },
-        ])
+        axios
+            .get("https://social-network.samuraijs.com/api/1.0/users")
+            .then((response) => {
+                props.setUsers(response.data.items)
+            })
     }
     const users = props.usersData.map((user) => {
         return (
             <div className={ s.users }>
                 <div key={ user.id } className={ s.userItem }>
                     <div className={ s.userAbout }>
-                        <img className={ s.userPhoto } src={ user.userPhoto } alt=""/>
+                        <img className={ s.userPhoto }
+                             src={ user.photos.small != null ? user.photos.small : userPhoto }
+                             alt=""
+                        />
                         <div className={ s.description }>
-                            <h1 className={ s.userName }>{ user.userName }</h1>
+                            <h1 className={ s.userName }>{ user.name }</h1>
                             <div className={ s.status }>{ user.status }</div>
                             <div className={ s.location }>
-                                <span>{ user.location.city }</span>
-                                <span>{ user.location.country }</span>
+                                <span>user.location.city</span>
+                                <span>user.location.country</span>
                             </div>
                         </div>
                     </div>
